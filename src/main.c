134fbe9ee15a7d2384ec254a7e17f3a0873af38a
root <douglaslaundy@gmail.com>
2025-04-16 13:41:57 +0000

    feat(#1): Format files

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 #include "../header_files/decompress.h"
 #include "../header_files/utils.h"
 
-char* compress_file = NULL; // Define variable for file to compress 
-char* decompress_file = NULL; // define variable for file to decompress 
+char *compress_file = NULL;   // Define variable for file to compress
+char *decompress_file = NULL; // define variable for file to decompress
 
 void print_help(const char *program_name)
 {
@@ -19,7 +19,8 @@ void print_help(const char *program_name)
     printf("  -b path/to/file.rle    A file to decompress (must be a .rle)\n");
 }
 
-void cleanup(){
+void cleanup()
+{
     free_pointer(compress_file);
     free_pointer(decompress_file);
 }
@@ -36,58 +37,76 @@ int main(int argc, char *argv[])
     int compress_flag = 0;   // Flag for compression
     int decompress_flag = 0; // Flag for decompression
     // Loop through options
-    while ((opt = getopt(argc, argv, "c:d:")) != -1) {
-        switch (opt) {
-            case 'c':
-                if (optarg) {
-                    printf("Option -c with value: %s\n", optarg);
-                    compress_flag = 1; // Set compression flag
-                    compress_file = malloc(strlen(optarg) + 1); // Reallocate the correct amount of memory
-                    if (compress_file == NULL) {
-                        perror("Memory allocation failed for compress_file: ");
-                        exit(EXIT_FAILURE);
-                    }
-                    strcpy(compress_file,optarg);
-                } else {
-                    printf("For -c option please provide a relative path to a .txt file.\n");
+    while ((opt = getopt(argc, argv, "c:d:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'c':
+            if (optarg)
+            {
+                printf("Option -c with value: %s\n", optarg);
+                compress_flag = 1;                          // Set compression flag
+                compress_file = malloc(strlen(optarg) + 1); // Reallocate the correct amount of memory
+                if (compress_file == NULL)
+                {
+                    perror("Memory allocation failed for compress_file: ");
                     exit(EXIT_FAILURE);
                 }
-                break;
-            case 'd':
-            if (optarg) {
-                    printf("Option -d with value: %s\n", optarg);
-                    decompress_flag = 1; // Set decompression flag
-                    decompress_file = malloc(strlen(optarg) + 1); // Reallocate the correct amount of memory
-                    if (decompress_file == NULL) {
-                        perror("Memory allocation failed for decompress_file: ");
-                        exit(EXIT_FAILURE);
-                    }
-                    strcpy(decompress_file,optarg);
-            } else {
+                strcpy(compress_file, optarg);
+            }
+            else
+            {
+                printf("For -c option please provide a relative path to a .txt file.\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'd':
+            if (optarg)
+            {
+                printf("Option -d with value: %s\n", optarg);
+                decompress_flag = 1;                          // Set decompression flag
+                decompress_file = malloc(strlen(optarg) + 1); // Reallocate the correct amount of memory
+                if (decompress_file == NULL)
+                {
+                    perror("Memory allocation failed for decompress_file: ");
+                    exit(EXIT_FAILURE);
+                }
+                strcpy(decompress_file, optarg);
+            }
+            else
+            {
                 printf("For -d option please provide a relative path to a .rle file.\n");
                 exit(EXIT_FAILURE);
             }
-                break;
-            case '?':
-                printf("Unknown option: -%c\n", optopt);
-                exit(EXIT_FAILURE);
-                break;
-        }}
-    if (decompress_flag && compress_flag) {
+            break;
+        case '?':
+            printf("Unknown option: -%c\n", optopt);
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+    if (decompress_flag && compress_flag)
+    {
         printf("Error: Cannot use both -c and -d options at the same time.\n");
         exit(EXIT_FAILURE);
     }
-    if (compress_flag) {
+    if (compress_flag)
+    {
         int compress_result = compress(compress_file);
-        if (compress_result != 0) {
+        if (compress_result != 0)
+        {
             printf("Error compressing file: %s\n", compress_file);
             exit(EXIT_FAILURE);
         }
-    } else if (decompress_flag) {
+    }
+    else if (decompress_flag)
+    {
         int decompress_result = decompress(decompress_file);
-        if (decompress_result != 0) {
+        if (decompress_result != 0)
+        {
             printf("Error decompressing file: %s\n", decompress_file);
             exit(EXIT_FAILURE);
         }
     }
     return 0;
+}
--- src/utils.c
+++ src/utils.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void free_pointer(char *pntr) {
-    if ( pntr != NULL ) {
+void free_pointer(char *pntr)
+{
+    if (pntr != NULL)
+    {
         free(pntr);
     }
 }
 
-void change_extension(char *file_name,const char *new_extension) {
+void change_extension(char *file_name, const char *new_extension)
+{
     strcpy(&file_name[strlen(file_name) - 4], new_extension);
 }
